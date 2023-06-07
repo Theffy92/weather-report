8eb51d1e4e3eb7382453d51d888d85881e969ff2
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-06-07 16:40:57 -0400

    add states index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 <script src="./node_modules/axios/dist/axios.min.js"></script>
 "use strict";
+
+const state ={
+    //HTML states
+    increaseTempControl: null,
+    tempValue: null,
+    decreaseTempControl:null
+};
