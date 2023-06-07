7f9781f7a916cf6ed53c201d44f51cbc06b0a48b
Barbara <barbaraelisabethb@gmail.com>
2023-06-07 19:36:37 -0400

    Wave 1 - clickable arrows

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-<script src="./node_modules/axios/dist/axios.min.js"></script>
 "use strict";
+
+const state ={
+  increaseTempControl: null,
+  tempValue: null,
+  decreaseTempControl:null,
+  tempValueCount: 20
+};
+
+const loadControls = () => {
+  state.increaseTempControl = document.getElementById("increaseTempControl");
+  state.tempValue = document.getElementById("tempValue");
+  state.decreaseTempControl = document.getElementById("decreaseTempControl");
+};
+
+const handleIncreaseTempBtnClick = () => {
+state.tempValueCount += 1;
+state.tempValue.textContent = state.tempValueCount;
+};
+
+const handleDecreaseTempBtnClick = () => {
+  state.tempValueCount -= 1;
+  state.tempValue.textContent = state.tempValueCount;
+  };
+
+const registerEvents = () => {
+state.increaseTempControl.addEventListener("click", handleIncreaseTempBtnClick);
+state.decreaseTempControl.addEventListener("click", handleDecreaseTempBtnClick);
+};
+
+const onLoad = () => {
+  loadControls();
+  registerEvents();
+};
+
+onLoad();
