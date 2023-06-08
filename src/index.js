9f1f0d7ef95f1c9dbbd4d827571ad6860f4a12c5
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-06-08 00:26:22 -0400

    Complete Wave 2, add some code to css file to manage the size of the image

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const state ={
   increaseTempControl: null,
   tempValue: null,
   decreaseTempControl:null,
+  landscape: null,
   //data
   tempValueCount: 20
 };
@@ -15,6 +16,7 @@ const loadControls = () => {
   state.increaseTempControl = document.getElementById("increaseTempControl");
   state.tempValue = document.getElementById("tempValue");
   state.decreaseTempControl = document.getElementById("decreaseTempControl");
+  state.landscape = document.getElementById("landscape");
 };
 
 const handleIncreaseTempBtnClick = () => {
@@ -31,26 +33,49 @@ const handleDecreaseTempBtnClick = () => {
   state.tempValue.textContent = state.tempValueCount;
   };
 
+const landscapeSelection = (img) => {
+  state.landscape.innerHTML = '';
+  const newImage = `assets/${img}.png`;
+  const newImageEl = document.createElement("img");
+  newImageEl.src = newImage;
+  newImageEl.alt = "a landscape";
+  // newImageEl.style.height = '500px';
+  // newImageEl.style.width = '700px';
+  state.landscape.prepend(newImageEl);
+};
+
 const handleChangeBackgroundColor = () => {
   const temp = state.tempValueCount;
   const valueColor = state.tempValue;
-  if(temp <= 9 && temp >= MIN_DEGREE){
+  
+  if(temp < 0 && temp >= MIN_DEGREE){
+    valueColor.style.backgroundColor = "paleturquoise";
+    //valueColor.style.color = "paleturquoise";
+    landscapeSelection("antarctica");
+  }else if(temp >= 0 && temp <= 9){
     valueColor.style.backgroundColor = "teal";
     //valueColor.style.color = "teal";
+    landscapeSelection("winter");
   }else if(temp >= 10 && temp <= 15){
     valueColor.style.backgroundColor = "green";
     //valueColor.style.color= "green";
-  }else if(temp >= 16 && temp <= 20){
+    landscapeSelection("autumn");
+  }else if(temp >= 16 && temp <= 24){
     valueColor.style.backgroundColor = "yellow";
     //valueColor.style.color = "yellow";
-  }else if(temp >= 21 && temp <= 25){
+    landscapeSelection("spring");
+  }else if(temp >= 25 && temp <= 34){
     valueColor.style.backgroundColor = "orange";
     //valueColor.style.color = "orange";
-  }else if(temp >= 26 && temp <= MAX_DEGREE){
+    landscapeSelection("summer");
+  }else if(temp >= 35 && temp <= MAX_DEGREE){
     valueColor.style.backgroundColor = "red";
     //valueColor.style.color = "red";
+    landscapeSelection("hot_weather");
   }
 };
+
+
 const registerEvents = () => {
   state.increaseTempControl.addEventListener("click", handleIncreaseTempBtnClick);
   state.decreaseTempControl.addEventListener("click", handleDecreaseTempBtnClick);
